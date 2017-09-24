0c0699e94d1db8a224265bd0a5ab518d31ccc230
isheebo <kedgar751@gmail.com>
2017-09-24 21:36:51 +0300

    add tests for livingspace

--- tests/test_room.py
+++ tests/test_room.py
@@ -24,3 +24,15 @@ class TestOffice(unittest.TestCase):
         self.assertEqual(self.office.members, [])
         self.assertEqual(self.office.type_, "Office")
         self.assertTrue(self.office.is_empty())
+
+
+class TestLivingSpace(unittest.TestCase):
+    def setUp(self):
+        self.livingspace = LivingSpace("kigali")
+
+    def test_is_created_livingspace(self):
+        self.assertEqual(self.livingspace.name, "Kigali")
+        self.assertEqual(self.livingspace.max_capacity, 4)
+        self.assertEqual(self.livingspace.type_, "LivingSpace")
+        self.assertEqual(self.livingspace.members, [])
+        self.assertTrue(self.livingspace.is_empty())
