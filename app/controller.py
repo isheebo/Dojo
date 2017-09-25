0c85b7367f00f1795e93c238da97d53efa32ba00
isheebo <kedgar751@gmail.com>
2017-09-25 09:47:02 +0300

    add the add_person functionality

--- app/controller.py
+++ app/controller.py
@@ -58,3 +58,67 @@ class Dojo:
         self.all_created_rooms[new_room.name] = new_room
         self.names_of_all_created_rooms.append(new_room.name)
         return True
+
+    def add_person(self, first_name, last_name, person_type, wants_accommodation):
+        """ add_person adds a person to the Dojo. If rooms are available,
+        it allocates the person a room according to their type"""
+        person_type = person_type.lower()
+        if person_type not in ("fellow", "staff"):
+            cprint("unknown person type: persons can be of either fellow or staff type")
+            return False
+
+        name = f"{first_name} {last_name}"
+
+        if name.title() in self.names_of_all_added_people:
+            cprint("person already exists: can't add them twice", color="red")
+            return False
+
+        wants_accommodation = True if wants_accommodation and wants_accommodation.lower() in ("yes", "y") else False
+
+        if wants_accommodation and person_type == "staff":
+            cprint("staff members cannot be accommodated at the Dojo's livingspaces", color="red")
+            return False
+
+        is_added = False
+
+        if person_type == "staff":
+            person = Staff(name)
+            cprint(f"{person.name} has been added successfully", color="blue")
+            self.added_people[person.name] = person
+            is_added = True
+            self.names_of_all_added_people.append(person.name)
+            if len(self.available_offices()) != 0:
+                office = random.choice(self.available_offices())
+                cprint(f"{first_name.title()} has been allocated office {office.name}", color="green")
+                office.members.append(person)
+                person.office_name = office.name
+                person.is_allocated = True
+            else:
+                cprint(f"{first_name.title()} has not been allocated an office yet", color="yellow")
+
+        if person_type == "fellow":
+            person = Fellow(name, wants_accommodation)
+            is_added = True
+            self.added_people[person.name] = person
+            self.names_of_all_added_people.append(person.name)
+            cprint(f"{person.name} has been added successfully", color="blue")
+            if len(self.available_offices()) != 0:
+                office = random.choice(self.available_offices())
+                cprint(f"{first_name.title()} has been allocated office {office.name}", color="green")
+                office.members.append(person)
+                person.office_name = office.name
+                person.is_allocated = True if not wants_accommodation else False
+            else:
+                cprint(f"{first_name.title()} has not been allocated an office yet", color="yellow")
+
+            if wants_accommodation:
+                if len(self.available_livingspaces()) != 0:
+                    livingspace = random.choice(self.available_livingspaces())
+                    cprint(f"{first_name.title()} has been allocated livingspace {livingspace.name}", color="blue")
+                    livingspace.members.append(person)
+                    person.livingspace_name = livingspace.name
+                    person.is_allocated = True if person.office_name and person.livingspace_name else False
+                else:
+                    cprint(f"{person.name} has not been allocated a living space as yet! create some rooms to add them",
+                           color="yellow")
+        return is_added
