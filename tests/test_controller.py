497b2bf68d05c17e02a9fdf72df363826ffe6bdc
isheebo <kedgar751@gmail.com>
2017-09-25 10:15:01 +0300

    test create_room fails when name of room already exists

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -10,3 +10,10 @@ class TestMainApp(unittest.TestCase):
         num_rooms = len(self.dojo.names_of_all_created_rooms)
         self.assertFalse(self.dojo.create_room("type", ["blue"]))
         self.assertEqual(len(self.dojo.names_of_all_created_rooms), num_rooms)
+
+    def test_create_room_fails_if_room_name_already_exists(self):
+        num_rooms = len(self.dojo.names_of_all_created_rooms)
+        self.assertTrue(self.dojo.create_room("office", ["blue"]))
+        self.assertEqual(len(self.dojo.names_of_all_created_rooms), num_rooms + 1)
+        self.assertFalse(self.dojo.create_room("livingspace", ["blue"]))
+        self.assertEqual(len(self.dojo.names_of_all_created_rooms), num_rooms + 1)
