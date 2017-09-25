d75b8308e5235e7912c76d6b127c8222ec5be294
isheebo <kedgar751@gmail.com>
2017-09-25 09:36:30 +0300

    Add models for staff and fellow

--- app/person.py
+++ app/person.py
@@ -6,3 +6,15 @@ class Person:
         self.is_allocated = False
         self.office_name = None
         self.type_ = self.__class__.__name__
+
+
+class Fellow(Person):
+    def __init__(self, name, wants_accommodation=False):
+        super(Fellow, self).__init__(name)
+        self.wants_accommodation = wants_accommodation
+        self.livingspace_name = None
+
+
+class Staff(Person):
+    def __init__(self, name):
+        super(Staff, self).__init__(name)
