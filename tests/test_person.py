f3a4078763a7a9ddcdb8a6d2dccc9f522f96da8a
isheebo <kedgar751@gmail.com>
2017-09-25 09:28:45 +0300

    test the Fellow model of Person

--- tests/test_person.py
+++ tests/test_person.py
@@ -12,3 +12,15 @@ class TestPerson(unittest.TestCase):
         self.assertFalse(self.person.is_allocated)
         self.assertIsNone(self.person.office_name)
         self.assertEqual(self.person.type_, "Person")
+
+
+class TestFellow(unittest.TestCase):
+    def setUp(self):
+        self.fellow = Fellow("billy baggins")
+
+    def test_fellow_is_created(self):
+        self.assertEqual(self.fellow.name, "Billy Baggins")
+        self.assertEqual(self.fellow.type_, "Fellow")
+        self.assertIsNone(self.fellow.office_name)
+        self.assertIsNone(self.fellow.livingspace_name)
+        self.assertFalse(self.fellow.is_allocated)
